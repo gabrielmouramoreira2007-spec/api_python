0fc1ad62c0399b6a6084b441cd958144e99371e5
Gabriel Moura Moreira <gabrielmouramoreira@MacBook-Air-de-Fabio-2.local>
2026-05-19 19:35:28 -0300

    meu primeiro commit e configuracao do actions

---
+++ main.py
@@ -0,0 +1,6 @@
+from fastapi import FastAPI
+app= FastAPI()
+@app.get("/health")
+def health_check():
+	return {"status":"ok","version":"1.0.0"}
+
---
+++ test_main.py
@@ -0,0 +1,7 @@
+from fastapi.testclient import TestClient
+from main import app 
+client = TestClient(app)
+def test_health_check():
+	response = client.get("/health")
+	assert response.status_code == 200
+	assert response.json()=={"status":"ok","version":"1.0.0"}
